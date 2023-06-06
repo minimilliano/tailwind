b96101b2c5818ed32dc9b8f36dd079c53c58e7e8
minimilliano <zumax98@gmail.com>
2023-06-06 16:33:54 +0100

    updated btn classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkmode: false,
   content: [],
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
   },
   plugins: [],
 }
